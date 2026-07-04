f475973eefc6117e131a9e8bb06c16aca21d2dd2
Yasir Farooqui <yasirfarooqui23645@gmail.com>
2026-07-04 14:56:04 +0530

    Change email link to use 'mailto:' protocol

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
             </Link>
             
             {/* Mail */}
-            <Link href="farooquiyasir12@gmail.com" className="hover:text-white transition-colors">
+            <Link href="mailto:farooquiyasir12@gmail.com" className="hover:text-white transition-colors">
               <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
             </Link>
             
